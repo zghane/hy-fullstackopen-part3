d39091a873c136fbd1a0869c4ff9a6650816c0e7
name <>
2022-11-01 01:42:42 -0400

    Part 3.10 - use port from env if supplied

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ app.post("/api/persons", (req, res) => {
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`)
 })
